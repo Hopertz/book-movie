81bfa4b6d458c30cddff0dac3c90667f0ba1104e
Hopertz <lugano.ngulwa@gmail.com>
2024-03-26 16:53:25 +0300

    fix typo

--- backend/pkg/mongodb/movie.go
+++ backend/pkg/mongodb/movie.go
@@ -3,6 +3,6 @@ package mongodb
 
 type Movie struct {
 	Name  string   `json:"name,omitempty" bson:"name,omitempty"`
-	Amount int32   `json:"amount,omitempty" bson:"name,omitempty"`
+	Amount int32   `json:"amount,omitempty" bson:"amount,omitempty"`
 	Seats [][]string `json:"seats,omitempty" bson:"seats,omitempty"`
 }
