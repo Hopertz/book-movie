b2d4cd8a7cd4999a8e753617871efccab44a188f
Hopertz <lugano.ngulwa@gmail.com>
2024-03-27 01:24:26 +0300

    update code

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,239 +1,154 @@
 <script lang="ts">
-  let selectedSeats: { row: number; seat: number }[] = [];
-  let totalPrice: number = 0;
-  let seatCount: number = 0;
+	import type { PageData } from './$types';
+	export let data: PageData;
 
-  const seatPrice: { [key: number]: number } = {
-    10: 10,
-    12: 12,
-    8: 8,
-    9: 9,
-  };
+	let selectedSeats: { row: number; seat: number }[] = [];
+	let totalPrice: number = 0;
+	let seatCount: number = 0;
 
-  let movie = { value: 10 }; // Default movie
-  $: total = totalPrice;
-  $: count = seatCount;
+	let booker_name: string = '';
+	let booker_phone: string = '';
 
-  let seats: ("" | "selected" | "occupied")[][] = [
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-  ];
+	let movie = { name: data.movies[0].name, value: data.movies[0].amount }; // Default movie
+	$: total = totalPrice;
+	$: count = seatCount;
 
-  function toggleSeat(row: number, seat: number): void {
-    if (seats[row][seat] === "selected") {
-      // Deselect seat
-      seats[row][seat] = "";
-      seatCount -= 1;
-      totalPrice -= seatPrice[movie.value];
-      selectedSeats = selectedSeats.filter(
-        (selectedSeat) => !(selectedSeat.row === row && selectedSeat.seat === seat)
-      );
-    } else if (seats[row][seat] === "") {
-      // Select seat
-      seats[row][seat] = "selected";
-      seatCount += 1;
-      totalPrice += seatPrice[movie.value];
-      selectedSeats.push({ row, seat });
-    }
-    seats = [...seats]; 
+	let seats = data.movies[0].seats;
 
-    console.log(seats)
-    console.log(row, seat)
-    console.log("selected",selectedSeats)
-  }
+	async function post_boker() {
+		const res = await fetch('/api/movie', {
+			method: 'POST',
+			body: JSON.stringify({
+				name: booker_name,
+				phone: booker_phone,
+				seat: selectedSeats,
+				movie: movie.name,
+				amount: movie.value,
+				seats: seats
+			}),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
 
-  function handleChange(event: Event): void {
-    const target = event.target as HTMLSelectElement;
-    movie.value = parseInt(target.value, 10);
-    totalPrice = 0;
-    seatCount = 0;
-    selectedSeats = [];
-    seats.forEach((row, rowIndex) => {
-      row.forEach((seat, seatIndex) => {
-        if (seat === "selected") {
-          seatCount += 1;
-          totalPrice += seatPrice[movie.value];
-          selectedSeats.push({ row: rowIndex, seat: seatIndex });
-        }
-      });
-    });
-  }
+		const b = await res.json();
+		console.log(b.seats);
+		booker_name = '';
+		booker_phone = '';
+	}
+
+	function toggleSeat(row: number, seat: number): void {
+		if (seats[row][seat] === 'selected') {
+			// Deselect seat
+			seats[row][seat] = '';
+			seatCount -= 1;
+			totalPrice -= movie.value;
+			selectedSeats = selectedSeats.filter(
+				(selectedSeat) => !(selectedSeat.row === row && selectedSeat.seat === seat)
+			);
+		} else if (seats[row][seat] === '') {
+			// Select seat
+			seats[row][seat] = 'selected';
+			seatCount += 1;
+			totalPrice += movie.value;
+			selectedSeats.push({ row, seat });
+		}
+		seats = [...seats];
+
+		console.log(seats);
+		console.log(row, seat);
+		console.log('selected', selectedSeats);
+	}
+
+	function handleChange(event: Event): void {
+		const target = event.target as HTMLSelectElement;
+		movie.name = target.value;
+
+		const selectedMovie = data.movies.find((m) => m.name === movie.name);
+
+		seats = [...(selectedMovie?.seats ?? [])];
+
+		movie.value = selectedMovie?.amount ?? 0;
+
+		totalPrice = 0;
+		seatCount = 0;
+		selectedSeats = [];
+		seats.forEach((row, rowIndex) => {
+			row.forEach((seat, seatIndex) => {
+				if (seat === 'selected') {
+					seatCount += 1;
+					totalPrice += movie.value;
+					selectedSeats.push({ row: rowIndex, seat: seatIndex });
+				}
+			});
+		});
+	}
 </script>
-  
-  <body>
-    <div class="movie-container">
-    <label for="movie">Pick a movie:</label>
-      <select id="movie" on:change={handleChange}>
-        <option value="10">Avengers: Endgame ($10)</option>
-        <option value="12">Joker ($12)</option>
-        <option value="8">Toy Story 4 ($8)</option>
-        <option value="9">The Lion King ($9)</option>
-      </select>
-    </div>
 
-    <ul class="showcase">
-      <li>
-        <div class="seat"></div>
-        <small>Unselected</small>
-      </li>
-      <li>
-        <div class="seat selected"></div>
-        <small>Selected</small>
-      </li>
-      <li>
-        <div class="seat occupied"></div>
-        <small>Occupied</small>
-      </li>
-    </ul>
+<body>
+	<div class="cinema">
+		<div class="movie-container">
+			<label for="movie">Pick a movie:</label>
+			<select id="movie" on:change={handleChange}>
+				{#each data.movies as movie}
+					<option value={movie.name}>{movie.name} (${movie.amount})</option>
+				{/each}
+			</select>
+		</div>
 
-    <div class="container">
-      <div class="screen"></div>
-      {#each seats as row, rowIndex}
-      <div class="row">
-        {#each row as seat, seatIndex}
-        <button 
-            class="seat {seat}" 
-            on:click={() => toggleSeat(rowIndex, seatIndex)}
-            title={`Row: ${rowIndex + 1}, Seat: ${seatIndex + 1}`}
-        >
-            <!-- <div style="font-size: 2;">12</div> -->
-        </button>
-        {/each}
-      </div>
-    {/each}
-  </div>
+		<ul class="showcase">
+			<li>
+				<div class="seat"></div>
+				<small>Unselected</small>
+			</li>
+			<li>
+				<div class="seat selected"></div>
+				<small>Selected</small>
+			</li>
+			<li>
+				<div class="seat occupied"></div>
+				<small>Occupied</small>
+			</li>
+		</ul>
 
-      
+		<div class="container">
+			<div class="screen"></div>
+			{#each seats as row, rowIndex}
+				<div class="row">
+					{#each row as seat, seatIndex}
+						<button
+							class="seat {seat}"
+							on:click={() => toggleSeat(rowIndex, seatIndex)}
+							title={`Row: ${rowIndex + 1}, Seat: ${seatIndex + 1}`}
+						>
+							<!-- <div style="font-size: 2;">12</div> -->
+						</button>
+					{/each}
+				</div>
+			{/each}
+		</div>
 
-    <p class="text">
-      You have selected <span id="count">{count}</span> seats for a price of $<span
-        id="total"
-        >{total}</span
-      >
-    </p>
+		<p class="text">
+			You have selected <span id="count">{count}</span> seats for a price of $<span id="total"
+				>{total}</span
+			>
+		</p>
+	</div>
 
+	<div class="classyform">
+		<h3>Works with voda number only (MPESA)</h3>
+		<label>
+			Name
+			<input bind:value={booker_name} type="text" />
+		</label>
+		<label>
+			Phone
+			<input bind:value={booker_phone} type="text" />
+		</label>
+		<button class="button-89" on:click={post_boker}>Buy</button>
+	</div>
 </body>
 
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Lato&display=swap");
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  background-color: #242333;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 80vh;
-  font-family: "Lato", sans-serif;
-  margin: 0;
-}
-
-.movie-container {
-  margin: 40px 0;
-}
-
-.movie-container select {
-  background-color: #fff;
-  border: 0;
-  border-radius: 5px;
-  font-size: 14px;
-  margin-left: 10px;
-  padding: 5px 15px 5px 15px;
-  -moz-appearance: none;
-  -webkit-appearance: none;
-  appearance: none;
-}
-
-.container {
-  perspective: 1000px;
-  margin-bottom: 30px;
-}
-
-.seat {
-  background-color: #444451;
-  height: 24px;
-  width: 20px;
-  margin: 3px;
-  border-top-left-radius: 6px;
-  border-top-right-radius: 6px;
-}
-
-.seat.selected {
-  background-color: #6feaf6;
-}
-
-.seat.occupied {
-  background-color: #fff;
-}
-
-.seat:nth-of-type(2) {
-  margin-right: 18px;
-}
-
-.seat:nth-last-of-type(2) {
-  margin-left: 18px;
-}
-
-.seat:not(.occupied):hover {
-  cursor: pointer;
-  transform: scale(1.2);
-}
-
-.showcase .seat:not(.occupied):hover {
-  cursor: default;
-  transform: scale(2);
-}
-
-.showcase {
-  background: rgba(0, 0, 0, 0.1);
-  padding: 5px 10px;
-  border-radius: 5px;
-  color: #777;
-  list-style-type: none;
-  display: flex;
-  justify-content: space-between;
-}
-
-.showcase li {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 0 10px;
-}
-
-.showcase li small {
-  margin-left: 2px;
-}
-
-.row {
-  display: flex;
-}
-
-.screen {
-  background-color: #fff;
-  height: 70px;
-  width: 100%;
-  margin: 15px 0;
-  transform: rotateX(-45deg);
-  box-shadow: 0 3px 10px rgba(255, 255, 255, 0.7);
-}
-
-p.text {
-  margin: 5px 0;
-}
-
-p.text span {
-  color: #6feaf6;
-}
-
-
+	@import '../styles.css';
 </style>
--- frontend/src/routes/api/movie/+server.ts
+++ frontend/src/routes/api/movie/+server.ts
@@ -1,7 +1,22 @@
-import { json } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-    const { a, b } = await request.json();
-    return json({ result: a + b });
+    const body = await request.json();
+    console.log(typeof body)
+   
+    const res = await fetch(`http://localhost:8448/bookers`, {
+        method: "POST",
+        headers: {
+				'content-type': 'application/json'
+			},
+            body: body
+
+    },)
+
+    if (res.ok) {
+       return res.json() 
+    }
+
+    return redirect(303, "/")
 };
