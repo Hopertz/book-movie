3e73aa1712951e4da4469a62120195a8ac6f6d55
Hopertz <lugano.ngulwa@gmail.com>
2024-04-14 17:14:30 +0300

    update  movie model include ID

--- backend/pkg/mongodb/movie.go
+++ backend/pkg/mongodb/movie.go
@@ -1,8 +1,10 @@
 package mongodb
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Movie struct {
-	Name  string   `json:"name,omitempty" bson:"name,omitempty"`
-	Amount int32   `json:"amount,omitempty" bson:"amount,omitempty"`
-	Seats [][]string `json:"seats,omitempty" bson:"seats,omitempty"`
+	ID     primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name   string             `json:"name,omitempty" bson:"name,omitempty"`
+	Amount int32              `json:"amount,omitempty" bson:"amount,omitempty"`
+	Seats  [][]string         `json:"seats,omitempty" bson:"seats,omitempty"`
 }
